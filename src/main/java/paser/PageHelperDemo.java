b903bc07439a2279367c67ec36f5032dfc5e2d5e
zhangjunjie <1152597680@qq.com>
2020-09-24 18:01:34 +0800

    2.1.0版本 解决反射获取不到字段权限bug 提升拦截等级demo

--- src/main/java/paser/PageHelperDemo.java
+++ src/main/java/paser/PageHelperDemo.java
@@ -1,16 +1,16 @@
 package paser;
 
-public class PageHelperDemo {
-/**
- *     @Autowired
- *     private List<SqlSessionFactory> sqlSessionFactoryList;
- *
- *     @PostConstruct
- *     public void addMyInterceptor() {
- *         EncryptionMybatisInterceptor e = new EncryptionMybatisInterceptor(new EncryptImpl());
- *         for (SqlSessionFactory sqlSessionFactory : sqlSessionFactoryList) {
- *             sqlSessionFactory.getConfiguration().addInterceptor(e);
- *         }
- *     }
- */
-}
+//@Configuration
+//@AutoConfigureAfter(PageHelperAutoConfiguration.class)
+//public class TestLogAutoConfiguration {
+//    @Autowired
+//    private List<SqlSessionFactory> sqlSessionFactoryList;
+//
+//    @PostConstruct
+//    public void addMyInterceptor() {
+//        EncryptionMybatisInterceptor e = new EncryptionMybatisInterceptor(new EncryptImpl());
+//        for (SqlSessionFactory sqlSessionFactory : sqlSessionFactoryList) {
+//            sqlSessionFactory.getConfiguration().addInterceptor(e);
+//        }
+//    }
+//}
